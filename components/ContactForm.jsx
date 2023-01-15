8f4ab8191c6411d87325e320d5cb7730687a900c
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-01-15 17:23:19 +0530

    added Contact Form

---
+++ components/ContactForm.jsx
@@ -0,0 +1,178 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import axios from "axios";
+
+const ContactForm = () => {
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  });
+  const [inputs, setInputs] = useState({
+    email: "",
+    message: "",
+  });
+  const handleServerResponse = (ok, msg) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg },
+      });
+      setInputs({
+        email: "",
+        message: "",
+      });
+    } else {
+      setStatus({
+        info: { error: true, msg: msg },
+      });
+    }
+  };
+  const handleOnChange = (e) => {
+    e.persist();
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    });
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+    axios({
+      method: "POST",
+      url: "https://formspree.io/f/xqkoaakp",
+      data: inputs,
+    })
+      .then((response) => {
+        handleServerResponse(
+          true,
+          "Thank you, your message has been submitted."
+        );
+      })
+      .catch((error) => {
+        handleServerResponse(false, error.response.data.error);
+      });
+  };
+  return (
+    <section className="text-gray-600 body-font relative">
+      <div className="container px-5 py-12 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Contact Us
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            In case of any queries, feel free to reach out to us.
+          </p>
+        </div>
+
+        <form onSubmit={handleOnSubmit}>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    htmlFor="name"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={handleOnChange}
+                    value={inputs.name}
+                    required
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    htmlFor="email"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="_replyto"
+                    onChange={handleOnChange}
+                    value={inputs.email}
+                    required
+                    className="w-full bg-gray-100 bg-opacity-50 rounded-md border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label
+                    htmlFor="message"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    onChange={handleOnChange}
+                    value={inputs.message}
+                    required
+                    className="w-full bg-white bg-opacity-50 rounded-md border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button
+                  disabled={status.submitting}
+                  className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 hover:bg-indigo-600 rounded text-lg "
+                >
+                  {!status.submitting
+                    ? !status.submitted
+                      ? "Submit"
+                      : "Submitted"
+                    : "Submitting..."}
+                </button>
+              </div>
+
+              <div className=" w-full mt-4 border-t border-gray-200 text-center">
+                <Link
+                  href="mailto:iste.vesit@ves.ac.in"
+                  className="text-indigo-500"
+                >
+                  iste.vesit@ves.ac.in
+                </Link>
+                <p className="leading-normal my-2">
+                  Hashu Adwani Memorial Complex, Collector's Colony,
+                  <br />
+                  Chembur, Mumbai, Maharashtra - 400074
+                </p>
+              </div>
+            </div>
+          </div>
+        </form>
+        {status.info.error && (
+          <div className="mt-4 text-red-600 flex justify-center">
+            Error: {status.info.msg}
+          </div>
+        )}
+        {!status.info.error && status.info.msg && (
+          <p className="mt-4 text-green-500 flex justify-center">
+            {status.info.msg}
+          </p>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
 import { Inter } from "@next/font/google";
+import ContactForm from "../components/ContactForm";
 import Layout from "../components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Layout title="Home"></Layout>;
+  return (
+    <Layout title="Home">
+      <ContactForm />
+    </Layout>
+  );
 }
