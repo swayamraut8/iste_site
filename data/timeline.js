d57498818360ee31843c342b5c5b30b1164dc86c
Sarvesh Patil <sarvesh2902@gmail.com>
2023-01-15 10:50:58 +0530

    updated timeline

--- data/timeline.js
+++ data/timeline.js
@@ -1,58 +1,30 @@
 const timeline = [
   {
     name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
+    desc: "Join us for the annual Tech Summit, where industry leaders will gather to discuss the latest advancements in technology and share their insights on the future of the field",
     date: "1st Jan, 2069",
+    img: "https://res.cloudinary.com/sarveshp46/image/upload/v1673158646/nothing-here_w38mzj.webp",
+    github: "https://github.com/ISTE-VESIT-ORG/Gitway-OpenSource",
+    instagram: "https://www.instagram.com/p/CmPK5WUPWh6/",
+    type: "tech",
   },
   {
     name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
+    desc: "Join us for the annual Tech Summit, where industry leaders will gather to discuss the latest advancements in technology and share their insights on the future of the field",
     date: "1st Jan, 2069",
+    img: "https://res.cloudinary.com/sarveshp46/image/upload/v1673158646/nothing-here_w38mzj.webp",
+    github: "https://github.com/ISTE-VESIT-ORG/Gitway-OpenSource",
+    instagram: "https://www.instagram.com/p/CmPK5WUPWh6/",
+    type: "non-tech",
   },
   {
     name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
+    desc: "Join us for the annual Tech Summit, where industry leaders will gather to discuss the latest advancements in technology and share their insights on the future of the field",
     date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
-  },
-  {
-    name: "AR Workshop",
-    desc: "lorem ipsum dolor sit amet, consectetur adip",
-    date: "1st Jan, 2069",
+    img: "https://res.cloudinary.com/sarveshp46/image/upload/v1673158646/nothing-here_w38mzj.webp",
+    github: "https://github.com/ISTE-VESIT-ORG/Gitway-OpenSource",
+    instagram: "https://www.instagram.com/p/CmPK5WUPWh6/",
+    type: "fun",
   },
 ];
 
