0559cf2aafe35a68d9b6672d5354686d53ca4d61
Sarvesh Patil <sarvesh2902@gmail.com>
2023-01-23 18:45:47 +0530

    added events desc and ui changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,10 @@ const Navbar = () => {
   return (
     <div className="relative w-full h-20  shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-7 2xl:px-16">
-        <div className="flex flex-row justify-around">
+        <Link
+          href="/"
+          className="flex flex-row justify-around hover:text-indigo-900 transition-all"
+        >
           <img
             src="https://res.cloudinary.com/atharva7/image/upload/v1671376589/samples/iste_logo_thwbwl.jpg"
             alt="ISTE_Logo"
@@ -20,7 +23,7 @@ const Navbar = () => {
           <h1 className="text-3xl cursor-pointer flex items-center ml-4">
             ISTE-VESIT
           </h1>
-        </div>
+        </Link>
 
         <div>
           <ul className="hidden md:flex ">
@@ -28,16 +31,24 @@ const Navbar = () => {
                         Home
                     </Link> */}
             <Link href="/">
-              <li className="font-bold ml-10 text-md uppercase">Home</li>
+              <li className="font-bold ml-10 text-md uppercase hover:text-indigo-900 transition-all">
+                Home
+              </li>
             </Link>
             <Link href="/events">
-              <li className="font-bold ml-10 text-md uppercase">Events</li>
+              <li className="font-bold ml-10 text-md uppercase hover:text-indigo-900 transition-all">
+                Events
+              </li>
             </Link>
             <Link href="/council">
-              <li className="font-bold ml-10 text-md uppercase">Council</li>
+              <li className="font-bold ml-10 text-md uppercase hover:text-indigo-900 transition-all">
+                Council
+              </li>
             </Link>
             <Link href="/timeline">
-              <li className="font-bold ml-10 text-md uppercase">Timeline</li>
+              <li className="font-bold ml-10 text-md uppercase hover:text-indigo-900 transition-all">
+                Timeline
+              </li>
             </Link>
           </ul>
           <div onClick={handleNav} className="md:hidden">
@@ -57,14 +68,14 @@ const Navbar = () => {
         >
           <div>
             <div className="flex w-full items-center h-20 justify-between">
-              <div className="flex flex-row">
+              <Link href="/" className="flex flex-row">
                 <img
                   src="https://res.cloudinary.com/atharva7/image/upload/v1671376589/samples/iste_logo_thwbwl.jpg"
                   alt="ISTE_Logo"
                   className="w-24 h-24 object-cover rounded-full pt-1 cursor-pointer"
                 />
                 {/* <h1 className="text-4xl">Portfolio</h1> */}
-              </div>
+              </Link>
 
               <div
                 onClick={handleNav}
@@ -81,7 +92,7 @@ const Navbar = () => {
           </div>
           <div>
             <ul className="uppercase font-bold">
-              <Link href="">
+              <Link href="/">
                 <li className="py-4 text-md">Home</li>
               </Link>
               <Link href="/events">
